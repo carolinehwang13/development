c5b097a09b2eb955042b63e68f8cd6493aca2507
carolinehwang13 <caroline_hwang@brown.edu>
2024-04-03 17:45:33 -0400

    filtering

--- src/App.js
+++ src/App.js
@@ -16,18 +16,51 @@ function App() {
   const [cartPrice, setCartPrice] = useState(0);
   const emptyCart = plantData.map(() => 0);
   const [cartContents, setCartContents] = useState(emptyCart);
+  const [sort, setSort] = useState("Default");
+  const [climateFilter, setClimateFilter] = useState("No Filter");
+  const [difficultyFilter, setDifficultyFilter] = useState("No Filter");
 
-  function addToCart (price, index){
-    setPrice(totalPrice + price);
-    setCart(
-      cart.map((value, idx) => {
-        if (idx == index) {
-          return value + 1;
-        } else {
-          return value;
+  // function addToCart(price, index) {
+  //   console.log("added");
+  //   setCartPrice(cartPrice + price);
+  //   setCartContents(
+  //     cartContents.map((value, idx) => {
+  //       if (idx == index) {
+  //         return value + 1;
+  //       } else {
+  //         return value;
+  //       }
+  //     })
+  //   );
+  // }
+
+  function displayCart() {
+    return cartContents
+      .map((amount, index) => {
+        if (amount > 0){
+          return (
+            <div className="cart-item">
+              <p>{amount}x </p>
+              <p>{idxtoitem[index]}</p>
+            </div>
+          )
         }
       })
-    );
+  }
+
+  function resetCart() {
+    setCartPrice(0);
+    setCartContents(emptyCart);
+  }
+
+  function changeClimateFilter() {
+    var climate = document.getElementById("climate-filter");
+    setClimateFilter(climate.value);
+  }
+
+  function changeDifficultyFilter() {
+    var difficulty = document.getElementById("difficulty-filter")
+    setDifficultyFilter(difficulty.value)
   }
 
   return (
@@ -39,59 +72,96 @@ function App() {
       </div>
       <div id="body">
         <div id="inventory">
-          {plantData.map((item, index) => (
-            <PlantItem
-              name={item.name}
-              image={item.image}
-              region={item.region}
-              difficulty={item.difficulty}
-              price={item.price}
-              totalPrice={cartPrice}
-              setPrice={setCartPrice}
-              cart={cartContents}
-              setCart={setCartContents}
-              index={index}
-            />
-          ))}
+          <div id="actions">
+            <h3>Sort:</h3>
+            <select id="sort">
+              <option>Default</option>
+              <option>Alphabetical (A to Z)</option>
+              <option>Price (low to high)</option>
+              <option>Price (high to low)</option>
+              <option>Difficulty</option>
+            </select>
+            <h3>Filters:</h3>
+            <div id="filter-option">
+              <p>Climate</p>
+              <select id="climate-filter" onChange={changeClimateFilter}>
+                <option value="No Filter">No Filter</option>
+                <option value="Desert">Desert</option>
+                <option value="Temperate">Temperate</option>
+                <option value="Cool">Cool</option>
+                <option value="Tropical">Tropical</option>
+              </select>
+            </div>
+            <div id="filter-option">
+              <p>Difficulty</p>
+              <select id="difficulty-filter" onChange={changeDifficultyFilter}>
+                <option value="No Filter">No Filter</option>
+                <option value="Easy">Easy</option>
+                <option value="Medium">Medium</option>
+                <option value="Hard">Hard</option>
+              </select>
+            </div>
+          </div>
+          <div id="plants">
+            {plantData.map((item, index) => (
+              <PlantItem
+                name={item.name}
+                image={item.image}
+                region={item.region}
+                difficulty={item.difficulty}
+                price={item.price}
+                totalPrice={cartPrice}
+                setPrice={setCartPrice}
+                cart={cartContents}
+                setCart={setCartContents}
+                index={index}
+                climateFilter={climateFilter}
+                difficultyFilter={difficultyFilter}
+              />
+            ))}
+          </div>
         </div>
         <div id="cart">
           <p>
             <b>Items:</b>
           </p>
-          {displayCart(cartContents)}
+          {displayCart()}
           <p>
             <b>Total:</b> ${cartPrice}
           </p>
+          <button onClick={resetCart}>Clear cart</button>
         </div>
       </div>
     </div>
   );
 }
 
-function addToCart(totalPrice, price, setPrice, cart, setCart){
-  setPrice(totalPrice + price);
-  setCart(
-    cart.map((value, idx) => {
-      if (idx == index) {
-        return value + 1;
-      } else {
-        return value;
-      }
-    })
-  );
-}
+// function addToCart(addToCart, cart){
+//   setPrice(totalPrice + price);
+//   setCart(
+//     cart.map((value, idx) => {
+//       if (idx == index) {
+//         return value + 1;
+//       } else {
+//         return value;
+//       }
+//     })
+//   );
+// }
 
-function displayCart(totalPrice, price, setPrice, cart, setCart) {
-  const filteredCart = cart.filter((amount) => amount > 0);
+// function displayCart(addToCart, cart) {
+//   const filteredCart = cart.filter((amount) => amount > 0);
 
-  return filteredCart.map((amount, index) => (
-    <div className="cart-item">
-      <button className="cart-button">+</button>
-      <p> {amount} </p>
-      <button className="cart-button">-</button>
-      <p>{idxtoitem[index]}</p>
-    </div>
-  ));
-}
+//   return filteredCart.map((amount, index) => (
+//     <div className="cart-item">
+//       <button className="cart-button" onClick={addToCart(2, index)}>
+//         +
+//       </button>
+//       <p> {amount} </p>
+//       <button className="cart-button">-</button>
+//       <p>{idxtoitem[index]}</p>
+//     </div>
+//   ));
+// }
 
 export default App;
--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,4 +1,4 @@
-export default function myPlantItem({ name, image, region, difficulty, price, totalPrice, setPrice, cart, setCart, index }) {
+export default function myPlantItem({ name, image, region, difficulty, price, totalPrice, setPrice, cart, setCart, index, climateFilter, difficultyFilter }) {
 
 
   
@@ -14,16 +14,52 @@ export default function myPlantItem({ name, image, region, difficulty, price, to
           })
         )
     }
-  
-  
-    return (
-    <div className="PlantItem">
-      <img id="plant-image" src={image} alt={name} />
-      <h2>{name}</h2>
-      <p>Climate: {region}</p>
-      <p>Difficulty: {difficulty}</p>
-      <p>${price}</p>
-      <button className="add-button" onClick={addToCart}>Add to cart</button>
-    </div>
-  );
+
+    function removeFromCart(){
+        setPrice(totalPrice - (price * cart[index]))
+        setCart(cart.map((value, idx) => {
+            if (idx == index) {
+                return 0;
+            }else {
+                return value;
+            }
+        }))
+    }
+
+    function inCart(){
+        if (cart[index] > 0){
+            return true
+        }else{
+            return false
+        }
+    }
+
+    if((climateFilter == "No Filter" || climateFilter == region) && (difficultyFilter == "No Filter" || difficultyFilter == difficulty)){
+        if(inCart()){
+            return (
+                <div className="PlantItem">
+                    <img id="plant-image" src={image} alt={name} />
+                    <h2>{name}</h2>
+                    <p>Climate: {region}</p>
+                    <p>Difficulty: {difficulty}</p>
+                    <p>${price}</p>
+                    <button className="add-button" onClick={addToCart}>Add to cart</button>
+                    <button onClick={removeFromCart}>Remove from cart</button>
+                </div>
+            )
+        }else{
+            return (
+                <div className="PlantItem">
+                    <img id="plant-image" src={image} alt={name} />
+                    <h2>{name}</h2>
+                    <p>Climate: {region}</p>
+                    <p>Difficulty: {difficulty}</p>
+                    <p>${price}</p>
+                    <button className="add-button" onClick={addToCart}>Add to cart</button>
+                </div>
+            )
+        }
+    }
+
+
 }
